48432004b68b0b6cc8d993954cfe00878ae54c38
nanisundara48-boop <nanisundara48@gmail.com>
2026-07-10 12:09:33 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js - API keys and Firebase Configuration
+export const OPENAI_API_KEY = "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";
+export const GEMINI_API_KEY = "AQ.Ab8RN6JgChPe_v-qNDgfDBNOi5yRriVWoU2JqN8BczlgAyKmEQ";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0LjnMxVedNp3JCZcdrztSq4Jx-w7MSWQ",
+  authDomain: "minibot-1b6dc.firebaseapp.com",
+  projectId: "minibot-1b6dc",
+  storageBucket: "minibot-1b6dc.firebasestorage.app",
+  messagingSenderId: "726010343364",
+  appId: "1:726010343364:web:5b46ff25ebe1b78620be91"
+};
